f0e94a523435d0e6a763af83ff89ec24655f6707
garaemon <garaemon@gmail.com>
2013-04-05 17:14:32 +0900

    #7: implementing preview view

--- routes/preview.js
+++ routes/preview.js
@@ -4,7 +4,6 @@ exports.post = function(req, res, next) {
     var name = req.param("name");
     var message = req.param("message");
     var location = req.param("location");
-
     var cookie_expire = new Date(Date.now() + 60 * 1000);
     if (!name || name.length == 0) {
         res.cookie("message", message, {expires: cookie_expire});
@@ -20,9 +19,11 @@ exports.post = function(req, res, next) {
         res.cookie("message", message, {expires: cookie_expire});
     }
     else {
+        // var newlined_message = message.split("\n").join("<br />");
+        // message.replaceAll("\n", "<br />");
         res.render("preview", {
             name: name,
-            message: message,
+            message: message.trim(),
             location: location
         });
     }
